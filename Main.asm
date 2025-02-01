6e242804a6acae75f9a88babde02e5122c50176f
MAEmamizade <aliem2004.3@gmail.com>
2025-02-02 01:32:28 +0330

    color done

--- Main.asm
+++ Main.asm
@@ -1,46 +1,157 @@
-PAINT MACRO COLOR	
-	MOV AH,0CH
-	MOV AL,COLOR
+;MACROS
+ERASE  	MACRO
+        PAINT_PIXEL  BLACK
+        INC         CX
+        PAINT_PIXEL  BLACK
+        INC         DX
+        PAINT_PIXEL  BLACK
+        DEC         DX 
+        DEC         DX         
+        PAINT_PIXEL  BLACK
+        DEC         CX
+        PAINT_PIXEL  BLACK
+        DEC         CX
+        PAINT_PIXEL  BLACK
+        INC         DX         
+        PAINT_PIXEL  BLACK
+        INC         DX         
+        PAINT_PIXEL  BLACK
+        INC         CX         
+        PAINT_PIXEL  BLACK
+ENDM
+;_____________________________________________________
+PAINT_PIXEL MACRO COLOR	
+			MOV AH,0CH
+			MOV AL,COLOR
+			INT 10H
+ENDM
+;_____________________________________________________
+COLOR_BAR  MACRO   COLOR, START_COL
+			LOCAL   COL_LOOP, ROW_LOOP
+			MOV     CX, START_COL + COLOR_MRGIN
+			COL_LOOP:
+				MOV     DX, COLOR_MRGIN
+				ROW_LOOP:
+					PAINT_PIXEL  COLOR
+					INC     DX
+					CMP     DX, COLOR_SIZE + COLOR_MRGIN
+					JB      ROW_LOOP
+
+					INC     CX
+					CMP     CX, COLOR_SIZE + START_COL + COLOR_MRGIN
+					JB      COL_LOOP
+ENDM
+;_____________________________________________________
+SET_COLOR    MACRO
+			LOCAL   SET_WHITE, SET_BLUE, SET_GREEN, SET_RED, END_CHOOSE
+			CMP 	DX, COLOR_MRGIN + COLOR_SIZE
+			JA 		END_CHOOSE
+				CMP     CX, COLOR_MRGIN + WHITE_COL + COLOR_SIZE
+				JB      SET_WHITE
+				CMP     CX, RED_COL + COLOR_MRGIN + COLOR_SIZE
+				JB      SET_RED
+				CMP     CX, GREEN_COL + COLOR_MRGIN + COLOR_SIZE
+				JB      SET_GREEN
+				CMP     CX, BLUE_COL + COLOR_MRGIN + COLOR_SIZE
+				JB      SET_BLUE
+				JMP     END_CHOOSE
+				
+				SET_WHITE:
+					MOV     PENCIL, WHITE
+					JMP     END_CHOOSE
+				SET_BLUE:
+					MOV     PENCIL, BLUE
+					JMP     END_CHOOSE
+				SET_GREEN:
+					MOV     PENCIL, GREEN
+					JMP     END_CHOOSE
+				SET_RED:
+					MOV     PENCIL, RED
+			END_CHOOSE:
+ENDM
+;_____________________________________________________
+SET_CURSOR	MACRO	ROW, COL
+			MOV		DH, ROW
+			MOV		DL, COL
+			MOV		BH, 0
+			MOV		AH, 2
+			INT		10H
+			ENDM
+;_____________________________________________________
+SET_VIDEO_MODE	MACRO
+				MOV		AX, 0012H
+				INT		10H
+				ENDM
+;_____________________________________________________
+SET_BACKGROUND_WHITE MACRO
+	MOV AH,0B
+	MOV BH,0
+	MOV BL,WHITE
 	INT 10H
+	INT 3H
 	ENDM
-	
+;_____________________________________________________
+
+
+
 .MODEL SMALL
-.STACK
+;___________________________STACK SEGMEMT____________________________________
+.STACK 64
+;___________________________DATA SEGMEMT____________________________________
 .DATA
 
-OLDVIDEO DB ?
-NEWVIDEO DB 12H
+		COLOR_SIZE  		EQU	20
+		COLOR_MRGIN			EQU 20
+
+		WHITE				EQU	00001111B
+		WHITE_COL			EQU	20
+
+		RED					EQU	00001100B
+		RED_COL				EQU	40
+
+		GREEN				EQU	00001010B
+		GREEN_COL			EQU	60
+
+		BLUE				EQU	00001001B
+		BLUE_COL			EQU	80
+
+		BLACK				EQU 00000000B
+
+		PENCIL			    DB  WHITE   
+;___________________________CODE SEGMEMT____________________________________
 .CODE
-MAIN PROC
-MOV AX,@DATA
-MOV DS,AX
-MOV AH,0FH ;get the current video mode
-INT 10H
-MOV OLDVIDEO,AL ;save it
-MOV AX,0600H ;clear screen
-MOV BH,07
-MOV CX,0
-MOV DX,184FH
-INT 10H
-MOV AH,00H ;set new video mode
-MOV AL,NEWVIDEO
-INT 10H
-MOV AX,0000H ;initialize mouse
-INT 33H
-MOV AX,01H
-INT 33H ;show mouse cursor
-BACK: MOV AX,03H ;check for mouse button press
-INT 33H
-CMP BX,0001H
-JNE BACK
-PAINT 4
-jmp BACK
-MOV AH,07
-INT 21H
-MOV AH,0
-MOV AL,OLDVIDEO
-INT 10H
-MOV AH,4CH
-INT 21H
-MAIN ENDP
-	END MAIN
+MAIN 	PROC
+		MOV AX,@DATA
+		MOV DS,AX
+;_____________________________PLAY GROUND____________________________________
+		SET_VIDEO_MODE
+		; SET_BACKGROUND_WHITE
+;_____________________________TOOLS BAR___________________________________
+		COLOR_BAR	WHITE, WHITE_COL
+		COLOR_BAR	RED, RED_COL
+		COLOR_BAR	GREEN, GREEN_COL
+		COLOR_BAR	BLUE, BLUE_COL
+;_____________________________DRAWING___________________________________
+
+		MOV AX,0000H
+		INT 33H
+		MOV AX,01H
+		INT 33H
+		BACK: MOV AX,03H
+			  INT 33H
+			  CMP BX, 02H
+			  JE  ERASER
+		  GO: SET_COLOR
+		      PAINT_PIXEL PENCIL
+			  jmp BACK
+			  ERASER: ERASE
+		      jmp GO
+		MOV AH,07
+		INT 21H
+
+MAIN 	ENDP
+		END MAIN
+		
+
+
+
