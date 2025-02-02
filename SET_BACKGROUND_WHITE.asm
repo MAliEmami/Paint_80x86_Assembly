200b679c5ecbbef22516ae6bc266751d5f5d9a12
amirhosseinbt <amirhosseinbrbt@gmail.com>
2025-02-02 21:35:52 +0330

    seperate macro from code

--- SET_BACKGROUND_WHITE.asm
+++
@@ -1,7 +0,0 @@
-SET_BACKGROUND_WHITE MACRO
-                     MOV AH,0B
-                     MOV BH,0
-                     MOV BL,WHITE
-                     INT 10H
-                     INT 3H
-ENDM
